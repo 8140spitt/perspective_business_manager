2dca52d9ab3ce779ba469019bbb44731ed5d74a5
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-06-16 00:09:09 +0100

    Add client accounts constants

---
+++ src/lib/packages/client-accounts/client-accounts.constants.ts
@@ -0,0 +1,5 @@
+export const clientAccountStatusCodes = ['ACTIVE', 'INACTIVE', 'ARCHIVED'] as const;
+
+export const clientAccountTypeCodes = ['STANDARD'] as const;
+
+export const clientAccountOnboardingStatusCodes = ['NEW', 'IN_PROGRESS', 'COMPLETE'] as const;
