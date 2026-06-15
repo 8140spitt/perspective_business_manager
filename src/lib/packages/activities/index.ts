45bc77712ee79ac8b0cb9eb138f1bd9c6cd87253
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-06-15 22:37:05 +0100

    Add activities package index

---
+++ src/lib/packages/activities/index.ts
@@ -0,0 +1,5 @@
+export * from './activities.constants';
+export * from './activities.repository.server';
+export * from './activities.service.server';
+export * from './activities.types';
+export * from './activities.validators';
