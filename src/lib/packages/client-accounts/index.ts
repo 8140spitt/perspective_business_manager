58470a1ce9c0de311758ab23c1d4ae4f21eed63e
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-06-16 00:10:41 +0100

    Add client accounts package index

---
+++ src/lib/packages/client-accounts/index.ts
@@ -0,0 +1,5 @@
+export * from './client-accounts.constants';
+export * from './client-accounts.repository.server';
+export * from './client-accounts.service.server';
+export * from './client-accounts.types';
+export * from './client-accounts.validators';
