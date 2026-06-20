a861d04a312c0339d18ea085763d7caa8cefbf58
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-06-20 21:58:39 +0100

    projects: export repository and service modules

--- src/lib/packages/projects/index.ts
+++ src/lib/packages/projects/index.ts
@@ -1,3 +1,5 @@
 export * from './projects.constants';
 export * from './projects.types';
 export * from './projects.validators';
+export * from './projects.repository.server';
+export * from './projects.service.server';
