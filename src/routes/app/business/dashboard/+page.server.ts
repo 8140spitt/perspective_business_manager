577aeab50970f7f6dee1e435aabc4e89fbaf0481
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-06-20 23:39:08 +0100

    Remove incomplete business dashboard route

--- src/routes/app/business/dashboard/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { getOrganisationWorkspace } from '$lib/packages/organisation/organisation.service.server';
-
-export async function load() {
-	return {
-		workspace: await getOrganisationWorkspace()
-	};
-}
